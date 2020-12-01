d237c5c9c55003879cb254cdfa0c48b224a532fc
Evan Freeze <efreeze@lyft.com>
2020-12-01 10:44:46 -0600

    day 1

---
+++ day-1/day-1.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+guard let rawInput = try? String(contentsOfFile: "./input.txt", encoding: .utf8) else {
+	fatalError("Failed to load input file")
+}
+
+let inputNumbers = rawInput.trimmingCharacters(in: .newlines).components(separatedBy: .newlines).map({ Int($0)! })
+
+func calculateResults(_ numbers: [Int]) -> Int {
+	for baseNumber in numbers {
+		for checkNumber in numbers {
+			if baseNumber == checkNumber {
+				continue
+			}
+			
+			let sum = baseNumber + checkNumber
+			if sum == 2020 {
+				return baseNumber * checkNumber
+			}
+		}
+	}
+	
+	return -1
+}
+
+let result = calculateResults(inputNumbers)
+print(result)
