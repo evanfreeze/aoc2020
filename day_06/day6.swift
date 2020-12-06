c98ce9b1514a10e1bb1de5d1475461a7f67d17d2
Evan Freeze <evanfreeze@gmail.com>
2020-12-06 22:20:50 +0000

    day 6 part 2

--- day_06/day6.swift
+++ day_06/day6.swift
@@ -4,12 +4,13 @@ guard let rawInput = try? String(contentsOfFile: "./input.txt", encoding: .utf8)
     fatalError("Failed to load input file")
 }
 
-let yesAnswers = rawInput
+let input = rawInput
     .trimmingCharacters(in: .newlines)
     .components(separatedBy: "\n\n")
-    .map({ $0.components(separatedBy: .newlines).joined() })
   
 // PART 1
+
+let yesAnswers = input.map({ $0.components(separatedBy: .newlines).joined() })
    
 var groupTotals = [Int]()
 
@@ -24,3 +25,25 @@ for answer in yesAnswers {
 let result = groupTotals.reduce(0, +)
 
 print("PART 1: Total yes answers across all groups is \(result)")
+
+// PART 2
+
+let yesesByGroup = input.map({ $0.components(separatedBy: .newlines) })
+
+var yesCountPerGroup = [Int]()
+
+for groupAnswers in yesesByGroup {
+    var questionsAnsweredYesByEveryoneInGroup = 0
+    
+    for questionLetter in groupAnswers.first! {
+        if groupAnswers.allSatisfy({ $0.contains(questionLetter) }) {
+            questionsAnsweredYesByEveryoneInGroup += 1
+        }
+    }
+    
+    yesCountPerGroup.append(questionsAnsweredYesByEveryoneInGroup)
+}
+
+let p2result = yesCountPerGroup.reduce(0, +)
+
+print("PART 2: Questions answered yes by everyone in group is \(p2result)")
